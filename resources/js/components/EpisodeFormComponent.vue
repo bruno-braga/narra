8c3be582d15bb2a604a4a19c2b721df0245c1504
bruno <bdcb@riseup.net>
2019-11-11 08:56:45 -0300

    Update episodeFormComponent to be used alone

--- resources/js/components/EpisodeFormComponent.vue
+++ resources/js/components/EpisodeFormComponent.vue
@@ -1,89 +1,64 @@
 <template>
-  <div class="card">
-    <div class="card-header">
-      {{ formTitle }}
+  <form @submit.prevent="submit">
+    <input type="hidden" name="_token" :value="token" />
+
+    <div class="form-group row">
+      <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
+
+      <div class="col-md-6">
+        <input v-model="episode.title" id="title" type="text" class="form-control" name="title" autocomplete="title" autofocus>
+      </div>
     </div>
 
-    <div class="card-body">
-      <form @submit.prevent="submit"> <input type="hidden" name="_token" :value="token" /> <div class="form-group row">
-          <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
+    <div class="form-group row">
+      <label for="title" class="col-md-4 col-form-label text-md-right">Program</label>
 
-          <div class="col-md-6">
-            <input v-model="episode.title" id="title" type="text" class="form-control" name="title" autocomplete="title" autofocus>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="title" class="col-md-4 col-form-label text-md-right">Program</label>
-
-          <div class="col-md-6">
-            <select class="form-control" v-model="episode.programId">
-              <option disabled value="">Choose a program</option>
-              <option v-for="program in programs" :value="program.id">{{ program.title }}</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="file" class="col-md-4 col-form-label text-md-right">File</label>
-
-          <div class="col-md-6">
-            <input ref="audio" name="file" type="file" @change="setFile($event)" />
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
-
-          <div class="col-md-6">
-            <textarea v-model="episode.description" name="description"></textarea>
-          </div>
-        </div>
-
-        <div class="form-group row mb-0">
-          <div class="col-md-6 offset-md-4">
-            <button class="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
+      <div class="col-md-6">
+        <select class="form-control" v-model="episode.programId">
+          <option disabled value="">Choose a program</option>
+          <option v-for="program in programs" :value="program.id">{{ program.title }}</option>
+        </select>
+      </div>
     </div>
-  </div>
+
+    <div class="form-group row">
+      <label for="file" class="col-md-4 col-form-label text-md-right">File</label>
+
+      <div class="col-md-6">
+        <input ref="audio" name="file" type="file" @change="setFile($event)" />
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+
+      <div class="col-md-6">
+        <textarea v-model="episode.description" name="description"></textarea>
+      </div>
+    </div>
+
+    <div class="form-group row mb-0">
+      <div class="col-md-6 offset-md-4">
+        <button class="btn btn-primary">
+          Submit
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
   import { required } from 'vuelidate/lib/validators';
-  import { eventBus } from '../eventBus';  
 
   export default {
-    name: 'formComponent',
+    name: 'episodeFormComponent',
     props: [
+      'data',
+      'isEdit',
       'programs',
       'route',
       'token'
     ],
-    mounted() {
-      eventBus.$on('populateForm', (res) => {
-        this.episode.title = res.title;
-        this.episode.programId = res.program_id;
-        this.episode.description = res.description;
-
-        this.formTitle = 'edit';
-        this.instanceRoute += `/${res.id}`;
-        this.form.append('_method', 'PUT');
-      });
-
-      eventBus.$on('setAddForm', (set) => {
-        this.form = new FormData()
-        this.formTitle = 'add';
-        this.instanceRoute = this.route;
-
-        for(let key in this.episode) {
-          this.episode[key] = null
-        }
-      });
-    },
     data() {
       return {
         episode: {
@@ -95,6 +70,18 @@
         formTitle: 'add',
         instanceRoute: this.route,
         form: new FormData()
+      }
+    },
+    created() {
+      if (this.data) {
+          this.episode.title = this.data.title;
+          this.episode.programId = this.data.program_id;
+          this.episode.description = this.data.description;
+      }
+
+      if (this.isEdit) {
+        this.instanceRoute += `/${this.data.id}`;  
+        this.form.append('_method', 'PUT');
       }
     },
     validations: {
@@ -125,8 +112,6 @@
           for(let key in this.episode) {
             this.episode[key] = null
           }
-
-          eventBus.$emit('updateEpisodeList', response)
         }
       }
     }
