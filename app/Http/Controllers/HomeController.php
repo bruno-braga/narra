696320f486cf86257df580331f8dcfa0b6bd153f
bruno <bdcb@riseup.net>
2019-11-28 15:26:36 -0300

    Add use program model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 use App\Rss\RssBuilder;
 
+
 use App\Repository\EpisodeRepositoryInterface;
 use App\Repository\ProgramRepositoryInterface;
+
+use App\Program;
 
 class HomeController extends Controller
 {
