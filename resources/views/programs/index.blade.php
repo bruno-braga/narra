46e2891128f7f1da81c3ea871be709ac3d191a99
bruno <bdcb@riseup.net>
2019-12-20 21:49:38 -0300

    Remove add episode button

--- resources/views/programs/index.blade.php
+++ resources/views/programs/index.blade.php
@@ -6,7 +6,6 @@
       <div class="col-md-12">
         <div>
           <a href="{{ url('/programs/create') }}" class="btn btn-primary float-right">Add Program</a>
-          <a href="{{ url('/episodes/create') }}" class="btn btn-primary float-right" style="margin-right: 10px">Add Episode</a>
         </div>
 
             <program-list-component :route="'{{ url('programs') }}'" :programs='@json($programs)'></list-component>
